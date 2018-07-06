180c2ecc15002187bdcdbc2d9ea705d650b1cda1
eduardohps <eduardohps@192.168.1.140>
2018-07-06 18:22:11 -0300

    Criado o Facade e Controller. Precisamos agr descidir os proximos passos.

---
+++ Lista_pra_mim-Project/src/Controllers/Controller.java
@@ -0,0 +1,52 @@
+package Controllers;
+
+/**
+ * Laboratório de Programação 2 - Lista pra mim© Project
+ * 
+ * 
+ * 
+ * @author Eduardo Henrique Pontes Silva - 117210360
+ */
+
+public class Controller {
+
+	// precisamos ver a questão do armazenamento, como não pode ter itens repitidos
+	// fica um pouco complicado ja que devemos localizar por identificador unico.
+
+	public int adicionaItemPorQtd(String nome, String categoria, int qnt, String unidadeDeMedid, String localDeCompra,
+			double preco) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int adicionaItemPorQuilo(String nome, String categoria, double kg, String localDeCompra, double preco) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int adicionaItemPorUnidade(String nome, String categoria, int qnt, String localDeCompra, double preco) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String exibeItem(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void atualizaItem(int id, String atributo, String novoValor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void adicionaPrecoItem(int id, String localDeCompra, double preco) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void deletaItem(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
