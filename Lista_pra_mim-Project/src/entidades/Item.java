58f75811e39782c8b6fbda8c1c8362fc252d40bf
gustavolbs <gustavo.santos@ccc.ufcg.edu.br>
2018-07-07 10:35:08 -0300

    Added the classes of item (no code yet)

---
+++ Lista_pra_mim-Project/src/entidades/Item.java
@@ -0,0 +1,19 @@
+package entidades;
+
+import java.util.Map;
+
+/**
+ * Laboratório de Programação 2
+ *
+ * @author Gustavo Santos - 117210400
+ * 
+ */
+public abstract class Item {
+
+	private int id;
+	private String nome;
+	private Map<String, Integer> precos;
+	
+	
+	
+}
---
+++ Lista_pra_mim-Project/src/entidades/ItemAlimentosNaoIndustrializados.java
@@ -0,0 +1,11 @@
+package entidades;
+
+/**
+ * 
+ *
+ * @author Gustavo Santos - 117210400
+ * 
+ */
+public class ItemAlimentosNaoIndustrializados extends Item {
+
+}
