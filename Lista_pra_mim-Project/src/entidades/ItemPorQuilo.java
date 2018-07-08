806ec6b8f1e1de231014aeef396348e8473252ea
João Pedro de Barros <joaopedro.bb98@gmail.com>
2018-07-08 15:14:08 -0300

    Adição de código nas classes filhas e correção de erros.

---
+++ Lista_pra_mim-Project/src/entidades/ItemPorQuilo.java
@@ -0,0 +1,80 @@
+package entidades;
+
+import java.util.HashMap;
+
+/**
+ * Laboratório de Programação 2 - Lista pra mim© Project
+ *
+ * @author Gustavo Santos - 117210400
+ * @author João Pedro de Barros - 117210327
+ * <p>
+ * Classe que representa um Item comercializado por quilo. Este tipo de item base possui nome, categoria,
+ * peso e precos. Nesta classe e possivel crir um item, adicionar um preco a um item, recuperar o nome do item,
+ * recuperar a categoria de um item, etc.
+ */
+
+public class ItemPorQuilo extends Item {
+
+    /**
+     * Double que representa o peso do item;
+     */
+    private double kg;
+
+    /**
+     * Método que cria um item comercializado por quilo.
+     *
+     * @param nome          String que representa o nome do item;
+     * @param categoria     String que representa a categoria do item;
+     * @param kg            Double que representa o peso do item;
+     * @param localDeCompra String que representa o local de compra do produto;
+     * @param preco         Double que representa o valor de compra do produto.
+     */
+    public ItemPorQuilo(String nome, String categoria, double kg, String localDeCompra, double preco) {
+        super.precos = new HashMap<>();
+
+        super.itemGenerico(nome, categoria, localDeCompra, preco);
+
+        if (kg < 0)
+            throw new IllegalArgumentException("Erro no cadastro de item: valor de quilos nao pode ser menor que zero.");
+
+        this.kg = kg;
+    }
+
+    /**
+     * Metodo que atualiza os dados de um item de acordo com o solicitado pelo
+     * usuario.
+     *
+     * @param atributo  Uma String que representa o atributo que sera atualizado.
+     * @param novoValor Uma String que representa o novo valor para o atributo.
+     */
+    @Override
+    public void atualizaItem(String atributo, String novoValor) {
+        super.atualizaItem(atributo, novoValor);
+
+        switch (atributo.trim().toLowerCase()) {
+            case "nome":
+            case "categoria":
+                break;
+
+            case "kg":
+                double kg = Double.parseDouble(novoValor);
+
+                if (kg < 0)
+                    throw new IllegalArgumentException("Erro na atualizacao de item: valor de quilos nao pode ser menor que zero.");
+                this.kg = kg;
+                break;
+
+            default:
+                throw new IllegalArgumentException("Erro na atualizacao de item: atributo nao existe.");
+        }
+    }
+
+    /**
+     * Metodo que retorna uma string contendo nome e categoria de um item.
+     *
+     * @return Uma String contendo nome e categoria de um item.
+     */
+    public String toString() {
+        return String.format("%s Preco por quilo: %s", super.toString(), super.getListaPrecos());
+    }
+}
---
+++ Lista_pra_mim-Project/src/entidades/ItemPorUnidade.java
@@ -0,0 +1,80 @@
+package entidades;
+
+import java.util.HashMap;
+
+/**
+ * Laboratório de Programação 2 - Lista pra mim© Project
+ *
+ * @author Gustavo Santos - 117210400
+ * @author João Pedro de Barros - 117210327
+ *
+ * Classe que representa um Item comercializado por unidade. Este tipo de item base possui nome, categoria,
+ * quantidade e precos. Nesta classe e possivel crir um item, adicionar um preco a um item,
+ * recuperar o nome do item, recuperar a categoria de um item, etc.
+ */
+public class ItemPorUnidade extends Item {
+
+    /**
+     * Quantidade de itens por compra.
+     */
+    private int qnt;
+
+    /**
+     * Método que cria um item comercializado por quilo.
+     *
+     * @param nome          String que representa o nome do item;
+     * @param categoria     String que representa a categoria do item;
+     * @param qnt           Inteiro que representa a quantidade de itens por compra;
+     * @param localDeCompra String que representa o local de compra do produto;
+     * @param preco         Double que representa o valor de compra do produto.
+     */
+    public ItemPorUnidade(String nome, String categoria, int qnt, String localDeCompra, double preco) {
+        super.precos = new HashMap<>();
+
+        super.itemGenerico(nome, categoria, localDeCompra, preco);
+
+        if (qnt < 0)
+            throw new IllegalArgumentException("Erro no cadastro de item: valor de unidade nao pode ser menor que zero.");
+
+        this.qnt = qnt;
+    }
+
+    /**
+     * Metodo que atualiza os dados de um item de acordo com o solicitado pelo
+     * usuario.
+     *
+     * @param atributo  Uma String que representa o atributo que sera atualizado.
+     * @param novoValor Uma String que representa o novo valor para o atributo.
+     */
+    @Override
+    public void atualizaItem(String atributo, String novoValor) {
+        super.atualizaItem(atributo, novoValor);
+
+        switch (atributo.trim().toLowerCase()) {
+            case "nome":
+            case "categoria":
+                break;
+
+            case "quantidade":
+            case "unidades":
+                int qnt = Integer.parseInt(novoValor);
+
+                if (qnt < 0)
+                    throw new IllegalArgumentException("Erro na atualizacao de item: valor de quantidade nao pode ser menor que zero.");
+                this.qnt = qnt;
+                break;
+
+            default:
+                throw new IllegalArgumentException("Erro na atualizacao de item: atributo nao existe.");
+        }
+    }
+
+    /**
+     * Metodo que retorna uma string contendo nome e categoria de um item.
+     *
+     * @return Uma String contendo nome e categoria de um item.
+     */
+    public String toString() {
+        return String.format("%s Preco: %s", super.toString(), super.getListaPrecos());
+    }
+}
