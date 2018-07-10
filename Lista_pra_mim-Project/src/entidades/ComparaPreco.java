1249a5cbb99397abb907049b49fb202703c06e89
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2018-07-09 21:39:22 -0300

    ajeitei a entidade comparapreco

---
+++ Lista_pra_mim-Project/src/entidades/ComparaPreco.java
@@ -0,0 +1,16 @@
+package entidades;
+import java.util.Comparator;
+/**
+*Esta classe ira usar o comparator para comparar os itens.
+*/
+public class ComparaPreco implements Comparator<Item> {
+	
+	/**
+	*Este metodo compara os itens pelo seu preco.
+	*/
+	@Override
+	public int compare(Item i1, Item i2) {
+		return (int) Math.ceil(i1.getMenorPreco() - i1.getMenorPreco());
+	}
+
+}
