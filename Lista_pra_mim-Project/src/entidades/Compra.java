6cfae0a856120b434e0fcb143a95f0ef116dd36a
joaobb <joao.pedro.barbosa@ufcg.edu.br>
2018-07-19 16:27:37 -0300

    Agora vai

--- Lista_pra_mim-Project/src/entidades/Compra.java
+++ Lista_pra_mim-Project/src/entidades/Compra.java
@@ -48,6 +48,31 @@ public class Compra {
 	public String getItemDescricao() {
 		return item.getDescricao();
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((item == null) ? 0 : item.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Compra other = (Compra) obj;
+		if (item == null) {
+			if (other.item != null)
+				return false;
+		} else if (!item.equals(other.item))
+			return false;
+		return true;
+	}
 
 	@Override
 	public String toString() {
