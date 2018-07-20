a2b7234030ffb15435405a765594d1f929df351b
gustavolbs <gusatvo.santos@ccc.ufcg.edu.br>
2018-07-20 13:55:01 -0300

    Terminando os testes de ListaDeCompras. CompraTest finalizado.

--- Lista_pra_mim-Project/tests/entidades/CompraTest.java
+++ Lista_pra_mim-Project/tests/entidades/CompraTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-<<<<<<< HEAD
 import Enum.Categoria;
 
 /**
@@ -272,35 +271,5 @@ public class CompraTest {
 		compra = new Compra(1, itemPU);
 		assertEquals("1 Creme dental Oral-C, higiene pessoal", compra.toString());
 	}
-=======
-/** 
- * Laboratório de Programação 2
- * 
- * @author Gustavo Santos - 117210400
- * @lab Laboratório 
- */
-public class CompraTest {
 
-	private Compra compra;
-	private Item itempqf;
-	private Item itempq;
-	private Item itempu;
-	
-	@Before
-	public void inicializa() {
-		itempqf = new ItemPorQuantidadeFixa("Agua Sanitaria Drogon", "limpeza", 1, "l", "Supermercado Excepcional", 2.19, 1);
-		itempq = new ItemPorQuilo("Peito de peru Saara", "alimento industrializado", 1.0, "Mercadinho Bem Barato", 34.49, 2);
-		itempu = new ItemPorUnidade("Creme dental Oral-C", "higiene pessoal", 3, "Mercadinho Bem Barato", 3.79, 3);
-	}
-	
-	@Test
-	public void testConstrutorValido() {
-		compra = new Compra(1, itempqf);
-	}
-	
-	
-	
-
-	
->>>>>>> 3b960362a4ab7c99c6f1f44b96325acd1ee0c86b
 }
